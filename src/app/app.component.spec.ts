a42e302cfdd0ece14fc3a68cf25941c6ce31b1ed
acalvom <andreac.3108@gmail.com>
2021-02-14 18:14:27 +0100

    delete obsolete code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,8 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach( () => {
+    TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
@@ -24,12 +24,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('testFront');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testFront app is running!');
   });
 });
