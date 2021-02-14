c0bc4235948341ec7f864828b1eb96a290f1ebdd
acalvom <andreac.3108@gmail.com>
2021-02-14 22:13:35 +0100

    fix sonar

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov' },
+        { type: 'lcovonly' },
       ]
     },
     reporters: ['progress', 'kjhtml'],
