8dddef8ff0252587e565e6b14bb8ab7c1bd5fae0
acalvom <andreac.3108@gmail.com>
2021-06-06 13:07:13 +0200

    fix heroku

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + PATH_PROJECT));
+// app.use(express.static(__dirname + PATH_PROJECT));
+app.use(express.static(__dirname + '/dist/test-front'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
 
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/test-front/index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => {
   if (!process.env.PORT) {
