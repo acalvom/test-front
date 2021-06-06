ef7211c3c0d9ed7fe4aa6e67f8a6570fc40e9b22
acalvom <andreac.3108@gmail.com>
2021-06-06 12:30:07 +0200

    fix Heroku configuration.

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 //Install express server
+const PATH_PROJECT = '/dist/test-front';
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/test-front'));
+app.use(express.static(__dirname + PATH_PROJECT));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/test-front/'}),
-);
+// app.get('/*', (req, res) =>
+//   res.sendFile('index.html', {root: 'dist/test-front/'}),
+// );
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
+
 
 app.listen(process.env.PORT || 8080, () => {
   if (!process.env.PORT) {
