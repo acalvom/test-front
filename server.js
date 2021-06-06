ac186c0f3d9feeb0040fa030c6b6cf0eb9826276
acalvom <andreac.3108@gmail.com>
2021-06-06 17:14:00 +0200

    fix heroku

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ app.use(express.static(__dirname + '/dist/test-front'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/test-front/index.html'));});
+
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, '/dist/test-front', 'index.html');
+  res.sendFile(index);
+});
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
+
+
 app.listen(process.env.PORT || 8080);
